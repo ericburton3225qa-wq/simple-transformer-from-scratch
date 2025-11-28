0ffac2ba6e08ddeea08a65082c403257a273e990
dragon <ericburton3225qa@gmail.com>
2025-11-28 08:41:47 -0800

    Add '__name__ == __main__' block to enable script execution and class import

--- model.py
+++ model.py
@@ -202,62 +202,62 @@ class TransformerLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
-
-# Initialize the model
-model = TransformerLanguageModel()
-model = model.to(device)
-
-
-# Get input embedding batch
-def get_batch(split: str):
-    data = train_data if split == 'train' else val_data
-    idxs = torch.randint(low=0, high=len(data) - context_length, size=(batch_size,))
-    x = torch.stack([data[idx:idx + context_length] for idx in idxs]).to(device)
-    y = torch.stack([data[idx + 1:idx + context_length + 1] for idx in idxs]).to(device)
-    return x, y
+if __name__== '__main__':
+    # Initialize the model
+    model = TransformerLanguageModel()
+    model = model.to(device)
 
 
-# Calculate loss
-@torch.no_grad()
-def estimate_loss():
-    out = {}
+    # Get input embedding batch
+    def get_batch(split: str):
+        data = train_data if split == 'train' else val_data
+        idxs = torch.randint(low=0, high=len(data) - context_length, size=(batch_size,))
+        x = torch.stack([data[idx:idx + context_length] for idx in idxs]).to(device)
+        y = torch.stack([data[idx + 1:idx + context_length + 1] for idx in idxs]).to(device)
+        return x, y
+
+
+    # Calculate loss
+    @torch.no_grad()
+    def estimate_loss():
+        out = {}
+        model.eval()
+        for split in ['train', 'valid']:
+            losses = torch.zeros(eval_iters)
+            for k in range(eval_iters):
+                x_batch, y_batch = get_batch(split)
+                logits, loss = model(x_batch, y_batch)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        model.train()
+        return out
+
+
+    # Use AdamW optimizer
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)
+    tracked_losses = list()
+    for step in range(max_iters):
+        if step % eval_iters == 0 or step == max_iters - 1:
+            losses = estimate_loss()
+            tracked_losses.append(losses)
+            print('Step:', step, 'Training Loss:', round(losses['train'].item(), 3), 'Validation Loss:',
+                round(losses['valid'].item(), 3))
+
+        xb, yb = get_batch('train')
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    # Save the model state dictionary
+    torch.save(model.state_dict(), 'model-ckpt.pt')
+
+    # Generate
     model.eval()
-    for split in ['train', 'valid']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            x_batch, y_batch = get_batch(split)
-            logits, loss = model(x_batch, y_batch)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
-
-# Use AdamW optimizer
-optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)
-tracked_losses = list()
-for step in range(max_iters):
-    if step % eval_iters == 0 or step == max_iters - 1:
-        losses = estimate_loss()
-        tracked_losses.append(losses)
-        print('Step:', step, 'Training Loss:', round(losses['train'].item(), 3), 'Validation Loss:',
-              round(losses['valid'].item(), 3))
-
-    xb, yb = get_batch('train')
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-
-# Save the model state dictionary
-torch.save(model.state_dict(), 'model-ckpt.pt')
-
-# Generate
-model.eval()
-start = 'The salesperson'
-start_ids = encoding.encode(start)
-x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
-y = model.generate(x, max_new_tokens=100)
-print('---------------')
-print(encoding.decode(y[0].tolist()))
-print('---------------')
+    start = 'The salesperson'
+    start_ids = encoding.encode(start)
+    x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+    y = model.generate(x, max_new_tokens=100)
+    print('---------------')
+    print(encoding.decode(y[0].tolist()))
+    print('---------------')
